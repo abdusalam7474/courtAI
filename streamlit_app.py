e96c4e3d77dfdcd36fd6d1d3f23865bf186900fe
abdusalam7474 <mideola824@gmail.com>
2024-07-19 17:21:30 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.compose import ColumnTransformer
 
+version = platform.python_version()
+st.write(version)
+
 st.set_page_config(page_title="Court Case Prediction", layout="wide")
 
 # (Replace with your actual machine learning model).
